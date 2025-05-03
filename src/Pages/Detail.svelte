789327a94a8f6d503d852ffa35f471b475638b39
QMartial <martial2210@gmail.com>
2025-05-03 10:57:26 +0200

    Tri carac pokemon

--- src/Pages/Detail.svelte
+++ src/Pages/Detail.svelte
@@ -3,10 +3,10 @@
   import type { Pokemon } from "../domain/entities/Pokemon";
   import { Api } from "../infrastructure/api/api";
   export let pokemon_name: string;
-  let pokemon_keys: string[];
-  let pokemon_values: object[];
-  let pokemon: Pokemon;
+  type NestedObject = { [key: string]: any };
+  let flattenPokemon: NestedObject = {};
   const api_pkmn = new Api("https://tyradex.app/api/v1/pokemon");
+
   async function get_one_pokemon() {
     const pokemon_list: Pokemon[] = await api_pkmn.getter();
     let p = pokemon_list.filter((pokemon) => {
@@ -16,28 +16,66 @@
     });
     return p[0];
   }
+
   onMount(async () => {
-    pokemon = await get_one_pokemon();
-    pokemon_keys = Object.keys(pokemon);
-    pokemon_values = Object.values(pokemon);
+    const pokemon = await get_one_pokemon();
+    flattenPokemon = flattenObject(pokemon);
+    console.log((flattenPokemon = flattenObject(pokemon)));
   });
+
+  function flattenObject(
+    obj: NestedObject,
+    parentKey = "",
+    result: NestedObject = {}
+  ): NestedObject {
+    for (const key in obj) {
+      if (obj.hasOwnProperty(key)) {
+        const newKey = parentKey ? `${parentKey}.${key}` : key;
+        if (
+          typeof obj[key] === "object" &&
+          obj[key] !== null &&
+          !Array.isArray(obj[key])
+        ) {
+          flattenObject(obj[key], newKey, result);
+        } else {
+          result[newKey] = obj[key];
+        }
+      }
+    }
+    return result;
+  }
 </script>
 
-<div
-  class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl"
+<main
+  class="min-h-screen bg-gradient-to-r from-red-200 via-yellow-100 to-yellow-50 flex flex-col items-center justify-center p-4"
 >
-  <div class="md:flex">
-    <div class="md:flex-shrink-0">
-      {#if pokemon}
-        pokemon_name={pokemon.name.fr}
-        {#each pokemon_keys as key, index}
+  <div class="mb-8">
+    <h1 class="text-4xl font-bold text-center text-red-600">
+      Détails du Pokémon
+    </h1>
+    <div
+      class="mb-8 grid grid-cols-1 gap-4 max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden p-4"
+    >
+      {#each Object.keys(flattenPokemon) as key}
+        {#if Array.isArray(flattenPokemon[key as keyof typeof flattenPokemon])}
           <div>
-            {key}:{JSON.stringify(pokemon_values[index])}
+            <strong>{key}</strong>
           </div>
-        {/each}
-      {:else}
-        Ce pokemon se trouve surement dans la prochaine génération
-      {/if}
+          {#each Object.values(flattenPokemon[key as keyof typeof flattenPokemon]) as keyArray}
+            <div>
+              {#each Object.values(keyArray as any[]) as i}
+                <span>{i}</span>
+              {/each}
+            </div>
+          {/each}
+        {:else}
+          <div>
+            <strong>{key}</strong>: {flattenPokemon[
+              key as keyof typeof flattenPokemon
+            ]}
+          </div>
+        {/if}
+      {/each}
     </div>
   </div>
-</div>
+</main>
--- src/domain/entities/Pokemon.ts
+++ src/domain/entities/Pokemon.ts
@@ -2,53 +2,71 @@ export interface Pokemon {
   pokedex_id: number;
   generation: number;
   category: string;
-  name: {
-    fr: string;
-    en: string;
-    jp: string;
-  };
-  sprites: {
-    regular: string;
-    shiny: string;
-    gmax: string | null;
-  };
-  types: {
-    name: string;
-    image: string;
-  }[];
-  talents: {
-    name: string;
-    tc: boolean;
-  }[];
-  stats: {
-    hp: number;
-    atk: number;
-    def: number;
-    spe_atk: number;
-    spe_def: number;
-    vit: number;
-  };
-  resistances: {
-    name: string;
-    multiplier: number;
-  }[];
-  evolution: {
-    pre: number | null;
-    next: {
-      pokedex_id: number;
-      name: string;
-      condition: string;
-    }[];
-    mega: number | null;
-  };
+  name: Name;
+  sprites: Sprites;
+  types: Type[];
+  talents: Talent[];
+  stats: Stats;
+  resistances: Resistance[];
+  evolution: Evolution;
   height: string;
   weight: string;
   egg_groups: string[];
-  sexe: {
-    male: number;
-    female: number;
-  };
+  sexe: Sexe;
   catch_rate: number;
   level_100: number;
   formes: string | null;
+}
+
+export interface Name {
+  fr: string;
+  en: string;
+  jp: string;
+}
+
+export interface Sprites {
+  regular: string;
+  shiny: string;
+  gmax: string | null;
+}
+
+export interface Type {
+  name: string;
+  image: string;
+}
+
+export interface Talent {
+  name: string;
+  tc: boolean;
+}
+
+export interface Stats {
+  hp: number;
+  atk: number;
+  def: number;
+  spe_atk: number;
+  spe_def: number;
+  vit: number;
+}
+
+export interface Resistance {
+  name: string;
+  multiplier: number;
+}
+
+export interface Evolution {
+  pre: number | null;
+  next: NextEvolution[];
+  mega: number | null;
+}
+
+export interface NextEvolution {
+  pokedex_id: number;
+  name: string;
+  condition: string;
+}
+
+export interface Sexe {
+  male: number;
+  female: number;
 }
